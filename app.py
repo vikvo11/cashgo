4c8420b0c4f8f9dc37c223199d0e8f4765bc7f25
vikvo11 <woroncull@yandex.ru>
2018-01-14 23:43:59 +0300

    s

--- app.py
+++ app.py
@@ -171,7 +171,8 @@ def mysqls():
     cur = mysql.connection.cursor()
     art='articles'
     # Get articles
-    result = cur.execute("SELECT * FROM %s",(art))
+    #result = cur.execute("SELECT * FROM %s",(art))
+    result = cur.execute("SELECT * FROM articles")
     #cur.execute("INSERT INTO articles(title,author,body) VALUES(%s,%s,%s)",(title,session['username'],body))
     articles = cur.fetchall()
     cur.close()
