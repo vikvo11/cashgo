1a924806a86cf58af5e6614539ed394ba97fc617
vikvo11 <woroncull@yandex.ru>
2018-01-14 23:47:25 +0300

    s

--- app.py
+++ app.py
@@ -169,7 +169,7 @@ def py():
 def mysqls():
     # Create cursor
     cur = mysql.connection.cursor()
-    username='articles'
+    username=articles
     # Get articles
     #result = cur.execute("SELECT * FROM %s",(art))
     #result = cur.execute("SELECT * FROM articles")
